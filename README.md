4a47a158e3ffa443756a43eb01a04e87afda4195
epaulf <ericpaulflorese@gmail.com>
2020-10-08 13:09:03 -0500

    primer commit

--- README.md
+++ README.md
@@ -1,24 +1,61 @@
-# resuelve
+# Prueba ingeniería Resuelve
 
-## Project setup
+Propuesta de solución al problema [Resuelve](https://github.com/resuelve/prueba-ing-backend/blob/master/README.md)
+
+## Manual de usuario
+
+- Descargar archivo Json para el [test](assets/json.txt)
+
+- Dar clic en el inpud de cargar archivo
+
+![Preview](uno.png "uno")
+
+- Seleccionar el archivo Json "json.txt"
+
+![Preview](dos.png "dos")
+
+- Una vez cargado el archivo json, dar clic en boton procesar archivo
+
+![Preview](tres.png "tres")
+
+- Revisar json obtenido
+
+![Preview](cuatro.png "cuatro")
+
+## Instalar dependencias
+
+2. Entrar a la raiz del proyecto y escribir el comando
+
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+## Compilar para producción
 
-### Compiles and minifies for production
+3. En línea de comandos escribir
+
 ```
 npm run build
 ```
 
+4. Al terminar el proceso de compilado se creara el directorio "dist"
+
+5. Copiar el contenido del directorio "dist" en la ruta de despliegues del servidor de aplicaciones
+
+## Para Desarrollo
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,143 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-card class="mx-auto">
+          <v-card-title>
+            <v-icon large left> mdi-code-json </v-icon>Prueba Ingeniería
+            Resuelve:</v-card-title
+          >
+          <v-file-input
+            accept=".txt"
+            v-model="file"
+            :rules="rulesfile"
+            @change="checkfile"
+            show-size
+            label="Cargar Archivo .txt con json a procesar"
+          ></v-file-input>
+          <v-card-actions>
+            <v-btn :disabled="procesa" @click="procesfile" block color="success"
+              >Procesar Archivo</v-btn
+            >
+          </v-card-actions>
+          <vue-json-pretty v-if="jsondata != null" :data="jsondata">
+          </vue-json-pretty>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="timeout"
+      :color="color"
+      :top="'top'"
+    >
+      {{ text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
+/*
+Exercise Resuelve
+Author: Eric Paul Flores Espinoza
+*/
+import VueJsonPretty from "vue-json-pretty";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    VueJsonPretty,
+  },
+  data: () => ({
+    file: [],
+    jsondata: null,
+    procesa: true,
+    rulesfile: [
+      (value) =>
+        !value ||
+        value.size < 2000000 ||
+        "Favor de seleccionar un archivo menos a 2 Mb!",
+    ],
+    // Individual team goals
+    niveles: {
+      rojo: { A: 5, B: 10, C: 15, CUAUH: 20 },
+      azul: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      verde: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      amarillo: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      blanco: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      negro: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      morado: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      naranja: { A: 8, B: 10, C: 15, CUAUH: 20 },
+      rosa: { A: 8, B: 10, C: 15, CUAUH: 20 },
+    },
+    snackbar: false,
+    text: "",
+    timeout: 5000,
+    color: "",
+  }),
+  methods: {
+    // Function to validate that the user uploads a file
+    checkfile(event) {
+      console.log(event);
+      if (event !== void 0 && event.size < 2000000) {
+        this.procesa = false;
+      } else {
+        this.procesa = true;
+        this.jsondata = null;
+      }
+    },
+    // Function that obtains the minimum goals according to their level of each player
+    getgoles(nivel, colorequipo) {
+      return this.niveles[colorequipo][nivel.toUpperCase()];
+    },
+    // Function that calculates the full salary of each player
+    getsueldo(item, colorequipo, golestotales) {
+      var metaindividual = this.getporcentaje(
+        item.goles,
+        this.niveles[colorequipo][item.nivel.toUpperCase()]
+      );
+      var metagolesequipo = Object.values(this.niveles[colorequipo]).reduce(
+        (a, b) => a + b
+      );
+      var metaequipo = this.getporcentaje(golestotales, metagolesequipo);
+      var porcentajebono = metaindividual / 2 + metaequipo / 2;
+      return (porcentajebono * item.bono) / 100;
+    },
+    // Function that obtains the percentage of individual goal
+    getporcentaje(goles, golesrequeridos) {
+      return (goles * 100) / golesrequeridos;
+    },
+    // Function that performs the calculations for each player
+    procesfile() {
+      var reader = new FileReader();
+      reader.readAsText(this.file);
+      reader.onload = () => {
+        try {
+          this.jsondata = JSON.parse(reader.result);
+          let equipo = null;
+          let golestotales = 0;
+          let equipos = [...new Set(this.jsondata.map((x) => x.equipo))];
+          equipos.forEach(function(itemcolor) {
+            equipo = this.jsondata.filter((x) => x.equipo === itemcolor);
+            golestotales = equipo.reduce((a, b) => a + b.goles, 0);
+            equipo.forEach(function(item) {
+              item.goles_minimos = this.getgoles(item.nivel, itemcolor);
+              item.sueldo_completo =
+                item.sueldo + this.getsueldo(item, itemcolor, golestotales);
+            }, this);
+          }, this);
+        } catch (error) {
+          this.snackbar = true;
+          this.color = "red";
+          this.text = "Archivo txt no contiene Json valido, revise su archivo";
+          this.procesa = true;
+          this.jsondata = null;
+        }
+      };
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
